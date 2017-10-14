b3d7a47edca5f74407af68cae8d100722102439a
southheat92 <southheat92@gmail.com>
2017-10-14 15:47:39 -0400

    beginning work on the hero service

---
+++ src/app/hero.service.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class HeroService {
+    getHeroes(): Hero[] {
+        return HEROES;
+    }
+}
