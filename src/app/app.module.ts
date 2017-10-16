4944042cc4b13007cef9981a4060049936f0e3a3
southheat92 <southheat92@gmail.com>
2017-10-15 20:40:55 -0400

    creating a new app.component.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { HeroService } from './hero.service';
+import { HeroesComponent } from './heroes.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
@@ -12,8 +14,10 @@ import { HeroDetailComponent } from './hero-detail.component';
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
+  providers: [HeroService],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
